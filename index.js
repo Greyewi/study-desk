02c30b552291723c7e88cfc31dca1df02d64f919
angli4anochka <Angli4anochka@icloud.com>
2023-01-27 11:03:33 +0300

    new

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const students = [{id: 1, name: 'Misha Sokolov', grade: 5}, {id: 2, name: 'Sasha Petrov', grade: 5}]
-const subjects = [{id: 1, name: 'Mathematics'}]
+const students = [
+  {id: 1, name: 'Камышев Леонид', grade: 8},
+  {id: 2, name: 'Ланграф Семён', grade: 8},
+  {id: 3, name: 'Чери Игорь ', grade: 8},
+  {id: 4, name: 'Тихон Снитко', grade: 8},
+  {id: 5, name: 'Пустогачев Амат', grade: 8},
+  {id: 6, name: 'Москаленко Сергей', grade: 8},
+  {id: 7, name: 'Махонин Савелий', grade: 8}
+]
+
+const subjects = [
+  {id: 1, name: 'Алгебра'},
+  {id: 2, name: 'Геометрия'},
+  {id: 3, name: 'Русский язык'},
+  {id: 4, name: 'Литератрура'},
+  {id: 5, name: 'Английский'},
+  {id: 6, name: 'История'},
+  {id: 7, name: 'Обществознание'},
+  {id: 8, name: 'Физика'},
+  {id: 9, name: 'ИЗО'},
+  {id: 10, name: 'Физкультура'},
+  {id: 11, name: 'Химия'},
+  {id: 12, name: 'География'},
+  {id: 13, name: 'Биология'},
+  {id: 14, name: 'ОБЖ'},
+  {id: 15, name: 'География Чукотки'},
+]
 
 const results = [
   {student_id: 1, subject_id: 1, mark: 5},
-  {student_id: 1, subject_id: 1, mark: 4},
-  {student_id: 1, subject_id: 1, mark: 4},
-  {student_id: 2, subject_id: 1, mark: 4}
+  {student_id: 1, subject_id: 2, mark: 4},
+  {student_id: 1, subject_id: 3, mark: 4},
+  {student_id: 1, subject_id: 4, mark: 2}
 ]
-
-
-
 
 const sumBy = (student_id, subject_id) => {
   return results.filter(f => f.student_id === student_id && f.subject_id === subject_id)
 }
+console.log(sumBy(1,2))
+
 
 const sumByStudent = (student_id) => (subject_id) => {
   return results.filter(f => f.student_id === student_id && f.subject_id === subject_id)
 }
+console.log(sumByStudent(1))
 
 const approximateMarksByStudents = (student_id) => {
   return results.reduce((prev, result) => {
@@ -29,20 +54,148 @@ const approximateMarksByStudents = (student_id) => {
   }, 0)
 }
 
+console.log(approximateMarksByStudents(1))
+
 students.forEach(student => {
   const getStudentMarks = sumByStudent(student.id)
   subjects.forEach(subject => {
-    console.log(getStudentMarks(subject.id))
+   
   })
 })
 
-
 const renderStudents = () => {
-  const table = document.querySelector('table thead')
+  const table = document.createElement('table')
+  const thead = document.createElement('thead')
+  const tableList = document.createElement('tr')
+  const subject_list = document.createElement('th')
+  const student_one = document.createElement('th')
+  const student_two = document.createElement('th')
+  const student_three = document.createElement('th')
+  const student_four = document.createElement('th')
+  const student_five = document.createElement('th')
+  const student_six = document.createElement('th')
+  const student_seven = document.createElement('th')
 
+  const tablePhotos = document.createElement('tr')
+  const firstTD = document.createElement('td')
+  const player_1 = document.createElement('td')
+  const player_2 = document.createElement('td')
+  const player_3 = document.createElement('td')
+  const player_4 = document.createElement('td')
+  const player_5 = document.createElement('td')
+  const player_6 = document.createElement('td')
+  const player_7 = document.createElement('td')
+
+  const tbody = document.createElement('tbody')
+  const tbadges = document.createElement('tr')
+  const TDbages = document.createElement('td')
+  const chemistryBadge = document.createElement('td')
+
+  const math = document.createElement('tr')
+  const math1 = document.createElement('td')
+  const math2 = document.createElement('td')
+  const geometry = document.createElement('tr')
+  const russian = document.createElement('tr')
+  const literature = document.createElement('tr')
+  const english = document.createElement('tr')
+  const history = document.createElement('tr')
+  const social_studies = document.createElement('tr')
+  const phisics = document.createElement('tr')
+  const art = document.createElement('tr')
+  const physical_culture = document.createElement('tr')
+  const geography = document.createElement('tr')
+  const biology = document.createElement('tr')
+  const safe_science = document.createElement('tr')
+  const geography_Chukotka = document.createElement('tr')
+  const chemistry = document.createElement('tr')
+
+
+  table.classList.add('table')
+  thead.classList.add('thead')
+  tableList.classList.add('table_list')
+  subject_list.classList.add('table_list')
+  student_one.classList.add('name', 'student_1')
+  student_two.classList.add('name',  'student_2')
+  student_three.classList.add('name', 'student_3')
+  student_four.classList.add('name', 'student_4')
+  student_five.classList.add('name',  'student_5')
+  student_six.classList.add('name', 'student_6')
+  student_seven.classList.add('name', 'student_7')
+
+  tbody.classList.add('tbody')
+  tbadges.classList.add('tbages')
+  chemistryBadge.classList.add('chemistry_badge')
+  TDbages.classList.add('TDbages')
+
+  player_1.classList.add('player_img1')
+  player_2.classList.add('player_img2')
+  player_3.classList.add('player_img3')
+  player_4.classList.add('player_img4')
+  player_5.classList.add('player_img5')
+  player_6.classList.add('player_img6')
+  player_7.classList.add('player_img7')
+
+  math.classList.add('math')
+  math1.classList.add('math1')
+  math2.classList.add('math2')
+  geometry.classList.add('geometry')
+  russian.classList.add('russian')
+  literature.classList.add('literature')
+  english.classList.add('english')
+  phisics.classList.add('phisics')
+  history.classList.add('history')
+  social_studies.classList.add('social_studies')
+  art.classList.add('art')
+  physical_culture.classList.add('physical_culture')
+  geography.classList.add('geography')
+  biology.classList.add('biology')
+  safe_science.classList.add('safe_science')
+  geography_Chukotka.classList.add('chukotka')
+  chemistry.classList.add('chemistry')
+
+
+  subject_list.textContent = "Ученики"
+  student_one.textContent = "Камышев Леонид"
+  student_two.textContent = "Ланграф Семен"
+  student_three.textContent = "Игорь Чери"
+  student_four.textContent = "Снитко Тихон"
+  student_five.textContent = "Пустогачев Амат"
+  student_six.textContent = "Москаленко Сергей"
+  student_seven.textContent = "Махонин Савелий"
+  firstTD.textContent = "Предметы"
+
+
+
+  math.textContent = subjects[0].name
+  geometry.textContent = subjects[1].name
+  russian.textContent = subjects[2].name
+  literature.textContent = subjects[3].name
+  english.textContent = subjects[4].name
+  history.textContent = subjects[5].name
+  social_studies.textContent = subjects[6].name
+  phisics.textContent = subjects[7].name
+  art.textContent = subjects[8].name
+  physical_culture.textContent = subjects[9].name
+  chemistry.textContent = subjects[10].name
+  geography.textContent = subjects[11].name
+  biology.textContent = subjects[12].name
+  social_studies.textContent = subjects[13].name
+  geography_Chukotka.textContent = subjects[14].name
+
+  math1.textContent = results[0].mark
+  math2.textContent = results[2].mark
+
+
+
+  document.getElementById('body')
+
+  table.append(thead, tablePhotos, tbadges, math, geometry, russian, literature, english, history, social_studies, phisics, art, physical_culture, chemistry, geography, biology, safe_science, geography_Chukotka)
+  thead.appendChild(tableList)
+  tableList.append(subject_list,student_one, student_two, student_three, student_four, student_five, student_six, student_seven)
+  tablePhotos.append( firstTD, player_1, player_2,player_3, player_4, player_5, player_6, player_7)
+  tbadges.append(TDbages,chemistryBadge)
+  math.append(math1, math2)
+  document.getElementById('body').appendChild(table);
 }
 
-const renderMarks = () => {
-  const table = document.querySelector('table tbody')
-
-}
+renderStudents()
